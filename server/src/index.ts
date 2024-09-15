279b29e9ec343c10f72a38fa06c5e7db79fb9b03
sharabh mishra <sharabhmishra15@gmail.com>
2024-09-15 19:04:45 +0530

    backend able to receive files and store them

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,36 +1,62 @@
 import express from "express";
 import { config } from "dotenv";
-import {OpenAI} from "openai";
+// import { Anthropic } from "@anthropic-ai/sdk";
+import multer from "multer"
+import path from "path"
 
 config();
 const app = express();
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-});
+app.use(express.urlencoded({extended: false}))
+// const anthropic = new Anthropic({
+//   apiKey: process.env.CLAUDE_API_KEY,
+// });
 
 const PORT = process.env.PORT || 3000;
+const storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: (req,file,cb)=>{
+    cb(null, `${Date.now()}-${file.originalname}`)
+  }
+})
+const upload = multer({
+  storage: storage,
+  limits: {fileSize: 1000000},
+  fileFilter: (req,file, cb)=> {
+    const fileTypes = /pdf|docx|doc/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = fileTypes.test(file.mimetype)
 
-
+    if(extname && mimetype){
+      return cb(null, true)
+    } else{
+      cb(new Error('file type not supported'))
+    }
+  }
+})
 
 app.get("/", async (req, res) => {
   try {
-    const response = await openai.chat.completions.create({
-      model: "gpt-4o-mini",
-    messages: [
-        { role: "system", content: "You are a helpful assistant." },
-        {
-            role: "user",
-            content: "Write a haiku about recursion in programming.",
-        },
-    ],
-  });
+    // const response = await anthropic.messages.create({
+    //   model: "claude-3-opus-20240229",
+    //   max_tokens: 1000,
+    //   messages: [
+    //     { role: "user", content: "Hello, Claude! How are you today?" },
+    //   ],
+    // });
 
-    console.log(response);
+    // console.log(response);
     res.send("Hello, I am working");
   } catch (e) {
     console.log(e);
   }
 });
+
+app.post('/upload',upload.single("document") ,async(req,res) => {
+  console.log(req.body);
+  console.log(req.file);
+  
+  res.send("received")
+})
 
 app.listen(PORT, () => {
   console.log(`server running on port: ${PORT}`);
